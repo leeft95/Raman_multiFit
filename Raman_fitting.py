709e30ffd1acb6486edee372f11fa6682793f390
leeft95 <leevan.trindade@gmail.com>
2019-02-16 04:03:30 +0000

    Add files via upload

---
+++ Raman_fitting.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 15 00:15:54 2019
+
+@author: leeva
+"""
+
+import os
+from sys import path
+path.append(os.getcwd() + "\\classes")
+import numpy as np
+import plot
+import matplotlib.pyplot as plt
+
+import fittingV_ver2
+class Data:
+    
+    def __init__(self,shift,signal,outfile,name,filename):
+        self.shift = shift
+        self.signal = signal
+        self.outfile = outfile
+        self.name = name
+        self.filename = filename
+        
+    
+       
+
+    
+folders = []
+files = []
+name = []
+spec = []
+pre = []
+rama = []
+m = []
+
+n = 10000
+cond = 0
+print('for all input 0 else input number of spectra: ')
+num = int(input())
+if num == 0:
+   cond = 1
+elif num == -1 or num == -11:
+    pre.append(str('p01_'))
+#    pre.append(str('p15_'))
+#    pre.append(str('p16_'))
+#    pre.append(str('p2_'))
+#    pre.append(str('p3_'))
+#    pre.append(str('p6_'))
+#    pre.append(str('p5_'))
+#    pre.append(str('p6_'))
+#    pre.append(str('p29_'))
+#    pre.append(str('p30_'))
+#    pre.append(str('p32_'))
+#    pre.append(str('p31_'))
+#    pre.append(str('p2_'))
+#    pre.append(str('p29_'))
+    cond = 0
+elif num !=0 or num != -1:
+    print('input spectra prefixes ie \' p0\' + CR for the appropriate number of files')
+    for i in range(num):
+        pre.append(input() + '_')
+        count = i
+        print('number of files ' + str(count+1) + ' of ' + str(num) + '\n')
+        if count+1 == num:
+            print('done')
+
+width = 50#int(input('Input the max peak width:\n'))
+n = 2#int(input('Input the number of peaks to fit:\n'))
+fit_type = 1#int(input('Input the fit type:\n 1 = Lorentzian'))
+#print(cond)
+        
+frmt = '1.txt'
+'''
+\\high pressure cell\\pressure 1
+'''
+if cond == 0:
+    for i in range(len(pre)):
+            for entry in os.scandir(path = os.getcwd() + "\\Mg(OH)2\\high pressure cell\\pressure 1"):
+                        if entry.is_dir():
+                            folders.append(entry.path)
+                        if (entry.name.startswith(pre[i]) and (entry.name.endswith(frmt) == 1)):
+                            files.append(entry.path)
+                            name.append(entry.name)
+if cond == 1:
+    for entry in os.scandir(path = os.getcwd() + "\\Mg(OH)2\\high pressure cell\\pressure 1"):
+                if entry.is_dir():
+                    folders.append(entry.path)
+                if entry.name.endswith(frmt) == 1:
+                    files.append(entry.path)
+                    name.append(entry.name)
+    
+#print(len(files))
+for i in range(len(files)):
+            infile = open(files[i], 'r')
+            outfile = 'new_' + os.path.splitext(name[i])[0] + '.png'
+            line = infile.readline()
+            data = np.loadtxt(infile)
+            shift = data[:,0]
+            signal = data[:,1]
+            
+            spec.append(Data(shift,signal,outfile,name[i],os.path.splitext(name[i])[0]))
+            
+            
+            rama.append(fittingV_ver2.Multi_fit(spec[i].shift,spec[i].signal,spec[i].name,n,width,fit_type))
+
+            
+            
+if num == -1:
+    sped = str(3600)
+    print(sped)
+    for i in range(len(spec)):
+        v = sped in spec[i].name
+        #d = 'ruby_after' in spec[i].name
+        if v == True:
+            m.append(i)
+elif num == -11:
+    sped = str(500)
+    print(sped)
+    for i in range(len(spec)):
+        v = sped in spec[i].name
+        #d = 'ruby_after' in spec[i].name
+        if v == True:
+            m.append(i)
+    
+else:
+    print('\nInput keyword of spectra:\n')
+    sped = str(input())
+    for i in range(len(spec)):
+        v = sped in spec[i].name
+        #d = 'ruby_after' in spec[i].name
+        if v == True:
+            m.append(i)
+        
+
+
+
+
+#plot.Plotter.draw_multi(rama,m)
+#for i in range(len(m)):
+
+
+#if sped == 'ruby_after':
+#    save_path = (os.getcwd() + '\\ruby_after\\' + sped + '_output data1.txt')
+#    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+#    output = open(sped + '_output data.txt','w')
+#    output.write('name\t shift\n')
+#    area,ruby = fittingV.Voigtfit.fitV(spec,n,m,rama,sped)
+#    for i in range(len(ruby)):
+#        output.write(ruby[i])
+if sped == '3600':
+    print('input number of peaks to fit: \n')
+    n = 2#int(input())
+    save_path = (str(os.getcwd()) + '\\3600cm\\' + sped + '_output data.txt')
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+    output = open(save_path,'w')
+    output.write('name\t peak_1\t peak_2\t area_1\t area_2\t area_total\t ratio\t fwhm_1\t fwhm_2\n')
+    for i in range(len(m)):
+        compound_x,compound_y,peak1_x,peak1_fit,peak2_x,peak2_fit,total_area,area_1,area_2,ratio = fittingV_ver2.Multi_fit.peaks_2(rama[m[i]])
+#    plt.plot(rama[m[0]].x,rama[m[0]].y)
+#    plt.show()
+#    plt.plot(compound_x,compound_y)
+#    plt.show()
+#    plt.plot(peak2_x,peak2_fit)
+#elif sped == '3300':
+#    print('input number of peaks to fit: \n')
+#    n = int(input())    
+#    save_path = (str(os.getcwd()) + '\\3300cm\\' + sped + '_output data.txt')
+#    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+#    output = open(save_path,'w')
+#    output.write('name\t peak_1\t peak_2\t area_1\t area_2\t area_total\t ratio\t fwhm_1\t fwhm_2\n')
+#    area,ruby = fittingV.Voigtfit.fitV(spec,n,m,rama,sped)
+#    for i in range(len(area)):
+#        output.write(area[i])
+#elif sped == '500':
+#    n = 2#input('input number of peaks to fit: \n')
+#    save_path = (str(os.getcwd()) + '\\500cm\\' + sped + '_output data.txt')
+#    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+#    output = open(save_path,'w')
+#    output.write('name\t area_1\t area_2\t area_total\t ratio\n')
+#    area,ruby = fittingV.Voigtfit.fitV(spec,n,m,rama,sped)
+#    for i in range(len(area)):
+#        output.write(area[i])
+output.close()
+    #plt.plot(x,y)
+    #plt.close()
+#root = peak_find(rama[m[0]].x,rama[m[0]].y,n)
+
+        
+        
+
---
+++ fittingV_ver2.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 14 23:28:51 2019
+
+@author: leeva
+"""
+
+import scipy
+import scipy.signal
+import scipy.interpolate as sci
+import matplotlib.pyplot as plt
+from scipy.integrate import simps
+import numpy as np
+from lmfit.models import (GaussianModel,LorentzianModel,PseudoVoigtModel,
+VoigtModel)
+
+
+class Multi_fit():
+    
+    def __init__(self,shift,signal,title,peaks,peak_width,fit_t):
+        
+        self.x = shift
+        self.y = signal
+        self.title = title
+        self.num_peaks = peaks
+        self.width = peak_width
+        self.type = fit_t
+    
+    def peaks_2(self):
+
+        
+        widths = np.arange(1,self.width,0.01)
+        
+        print(self.title)
+        
+        ####_find main peak_####
+        
+        smooth_indexes_scipy = scipy.signal.find_peaks_cwt(self.y, widths)
+        peak_indexes_xs_ys = np.asarray([list(a) for a in list(zip(self.x[smooth_indexes_scipy], self.y[smooth_indexes_scipy]))])
+        high_peak = sorted(peak_indexes_xs_ys, key=lambda pair: pair[1])[-self.num_peaks:]
+        print('\n' + str(high_peak[-1][0]) +  '\n')
+        
+        ####_normalisation lists_####
+        
+        fit_X = []
+        fit_Y = []
+        
+        fit_XX = []
+        fit_YY = []
+        
+        fit_X_p1 = []
+        fit_Y_p1 = []
+        
+        fit_X_p2 = []
+        fit_Y_p2 = []
+        
+        ####_cut down peak search area_####
+        
+        fit_range = 100
+        
+        for i in range(len(self.x)):
+            if self.x[i] > (high_peak[-1][0]-fit_range) and self.x[i] < (high_peak[-1][0]+fit_range):
+                #norm = (self.y[i] - min(self.y))/(max(self.y) - min(self.y))
+                fit_X.append(self.x[i])
+                fit_Y.append(self.y[i])
+        
+        
+        fit_X = np.asarray(fit_X)
+        fit_Y = np.asarray(fit_Y)
+        
+        ####_find second peak in cut down range and normalise data_####
+        
+        smooth_indexes_scipy = scipy.signal.find_peaks_cwt(fit_Y, widths)        
+        peak_indexes_xs_ys = np.asarray([list(a) for a in list(zip(fit_X[smooth_indexes_scipy], fit_Y[smooth_indexes_scipy]))])
+        high_peak2 = sorted(peak_indexes_xs_ys, key=lambda pair: pair[1])[-self.num_peaks:]
+        print(high_peak2)
+    
+        for i in range(len(fit_X)):
+            if fit_X[i] > high_peak[-1][0]-fit_range and fit_X[i] < high_peak[-1][0]+fit_range:
+                x  = (fit_Y[i] - min(fit_Y))/(max(fit_Y) - min(fit_Y))
+                fit_XX.append(fit_X[i])
+                fit_YY.append(x)
+                
+        fit_XX = np.asarray(fit_XX)
+        fit_YY = np.asarray(fit_YY)
+        
+        
+        ####_comopund fit_####
+        
+        if self.type == 1:
+            print('l')
+            lor_fit1 = LorentzianModel(prefix = 'v1_')
+            lor_fit2 = LorentzianModel(prefix = 'v2_')
+            
+        elif self.type == 2:
+            print('v')
+            lor_fit1 = VoigtModel(prefix = 'v1_')
+            lor_fit2 = VoigtModel(prefix = 'v2_')
+        
+        pars = lor_fit1.guess(fit_YY,x = fit_XX)
+        
+        pars['v1_center'].set(high_peak2[-1][0], min=high_peak[-1][0]-2, max=high_peak2[-1][0]+2)
+    
+        pars.update(lor_fit2.make_params())
+        
+        pars['v2_center'].set(high_peak2[0][0], min=high_peak2[0][0]-5, max=high_peak2[0][0]+5)
+        
+        compound = lor_fit1 + lor_fit2
+        
+        compound_fit = compound.fit(fit_YY,pars,x = fit_XX)
+        
+        total_area = simps(compound_fit.best_fit,dx =0.5)
+        
+        compound_x  = fit_XX
+        compound_y  = compound_fit.best_fit
+        
+        plt.plot(compound_x,fit_YY,label = 'best fit')
+        plt.plot(compound_x,compound_fit.best_fit,label = 'best fit')
+        
+
+        ####_single peak fits_####
+        
+        for i in range(len(fit_XX)):
+            if fit_XX[i] >  compound_fit.params['v1_center'].value-(fit_range/2) and fit_XX[i] <  compound_fit.params['v1_center'].value+(fit_range/2):
+                fit_X_p1.append(fit_XX[i])
+                fit_Y_p1.append(fit_YY[i])
+            if fit_XX[i] >  compound_fit.params['v2_center'].value-(fit_range/2) and fit_XX[i] <  compound_fit.params['v2_center'].value+(fit_range/2):
+                fit_X_p2.append(fit_XX[i])
+                fit_Y_p2.append(fit_YY[i])
+                
+        fit_X_p1 = np.asarray(fit_X_p1)
+        fit_Y_p1 = np.asarray(fit_Y_p1)
+        
+        fit_X_p2 = np.asarray(fit_X_p2)
+        fit_Y_p2 = np.asarray(fit_Y_p2)
+        
+        peak1 = lor_fit1.guess(fit_Y_p1,x = fit_X_p1)
+        peak2 = lor_fit2.guess(fit_Y_p2,x = fit_X_p2)
+        
+        peak1['v1_center'].set(compound_fit.params['v1_center'].value, min=compound_fit.params['v1_center'].value-5, max=compound_fit.params['v1_center'].value+5)  
+        
+        peak2['v2_center'].set(compound_fit.params['v2_center'].value, min=compound_fit.params['v2_center'].value-5, max=compound_fit.params['v2_center'].value+5)
+        peak2['v2_amplitude'].set(value =compound_fit.params['v2_amplitude'].value,vary = False)# ,min=3,max=7)
+        peak2['v2_sigma'].set(value = compound_fit.params['v2_sigma'].value, vary = False)
+        
+        peak1_fit = lor_fit1.fit(fit_Y_p1,peak1,x = fit_X_p1)
+        peak2_fit = lor_fit2.fit(fit_Y_p2,peak2,x = fit_X_p2)
+    
+        area_1 = simps(peak1_fit.best_fit, dx=0.5)
+        area_2 = simps(peak2_fit.best_fit, dx=0.5)
+        
+        plt.plot(fit_X_p1,peak1_fit.best_fit)
+        plt.plot(fit_X_p2,peak2_fit.best_fit)
+
+        plt.show()
+        plt.close()
+        ratio = area_1/area_2
+        
+        peak1_x = fit_X_p1
+        peak2_x = fit_X_p2
+        
+        
+        return compound_x,compound_y,peak1_x,peak1_fit.best_fit,peak2_x,peak2_fit.best_fit,total_area,area_1,area_2,ratio
+            
+            
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
