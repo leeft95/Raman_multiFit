182e038b5e62286c6f68d4affc3037636168b0d7
leeft95 <leevan.trindade@gmail.com>
2019-05-27 16:28:47 +0100

    Add files via upload
    Change Log:
    +updated error exceptions for all input parameters
    +added three peak fitting functionality
    +added support for .dat and .txt files exported from fityk and SpecView software respectively

--- Raman_fitting.py
+++ Raman_fitting.py
@@ -3,16 +3,21 @@
 Created on Fri Feb 15 00:15:54 2019
 
 @author: leeva
+
+
+
+
 """
 
 import os
 from sys import path
-path.append(os.getcwd() + "\\classes")
 import numpy as np
-import plot
-import matplotlib.pyplot as plt
-
 import fittingV_ver2
+
+'''
+Dependency: lmfit,numpy,scipy,matplotlib
+'''
+
 class Data:
     
     def __init__(self,shift,signal,outfile,name,filename):
@@ -21,9 +26,6 @@ class Data:
         self.outfile = outfile
         self.name = name
         self.filename = filename
-        
-    
-       
 
     
 folders = []
@@ -36,27 +38,37 @@ m = []
 
 n = 10000
 cond = 0
-print('for all input 0 else input number of spectra: ')
-num = int(input())
-if num == 0:
-   cond = 1
-elif num == -1 or num == -11:
-    pre.append(str('p1_'))
-#    pre.append(str('p15_'))
-#    pre.append(str('p16_'))
-#    pre.append(str('p2_'))
-#    pre.append(str('p3_'))
-#    pre.append(str('p6_'))
-#    pre.append(str('p5_'))
-#    pre.append(str('p6_'))
-#    pre.append(str('p29_'))
-#    pre.append(str('p30_'))
-#    pre.append(str('p32_'))
-#    pre.append(str('p31_'))
-#    pre.append(str('p2_'))
-#    pre.append(str('p29_'))
-    cond = 0
-elif num !=0 or num != -1:
+
+while True:
+    try:
+        data_path = input('Input data path: \n')
+        if os.path.isdir(data_path) != True:
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print('Please input valid path')
+
+while True:
+    try:
+        frmt = input('Input file format (form ".txt" or ".dat"): \n')
+        if frmt != '.dat' and frmt != '.txt':
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print('please input a valid file path')
+
+
+while True:    
+    try:
+        num = int(input('for all input 0 else input number of spectra: \n'))
+        break
+    except ValueError:
+        print('Please input an integer')
+        
+
+if num !=0:
     print('input spectra prefixes ie \' p0\' + CR for the appropriate number of files')
     for i in range(num):
         pre.append(input() + '_')
@@ -65,130 +77,129 @@ elif num !=0 or num != -1:
         if count+1 == num:
             print('done')
 
-width = int(input('Input the max peak width:\n'))
-n = int(input('Input the number of peaks to fit:\n'))
-fit_type = int(input('Input the fit type:\n 1 = Lorentzian\n 2 = Voigt\n'))
-#print(cond)
-        
-frmt = '1.txt'
-'''
-\\high pressure cell\\pressure 1
-'''
-if cond == 0:
+while True:
+    try:
+        width = int(input('Input the max peak width:\n'))
+        break
+    except ValueError:
+        print('please input a valid number')
+
+while True:
+    try:
+        fit_t = int(input('Input the number of peaks to fit \n 2 Peaks == 2 \n 3 Peaks == 3:\n'))
+        if fit_t != 2 and fit_t != 3:
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print('please select a valid option')
+
+while True:
+    try:
+        fit_type = int(input('Input the fit type:\n 1 = Lorentzian\n 2 = Voigt\n'))
+        if fit_type != 1 and fit_type != 2:
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print('please select a valid option')
+
+if num != 0:
     for i in range(len(pre)):
-            for entry in os.scandir(path = os.getcwd() + "\\Mg(OH)2\\high pressure cell\\pressure 1"):
+            for entry in os.scandir(path = data_path):
                         if entry.is_dir():
                             folders.append(entry.path)
                         if (entry.name.startswith(pre[i]) and (entry.name.endswith(frmt) == 1)):
                             files.append(entry.path)
                             name.append(entry.name)
-if cond == 1:
-    for entry in os.scandir(path = os.getcwd() + "\\Mg(OH)2\\high pressure cell\\pressure 1"):
+                            print(entry.name)
+else:
+    for entry in os.scandir(path = data_path):
                 if entry.is_dir():
                     folders.append(entry.path)
-                if entry.name.endswith(frmt) == 1:
+                if ((entry.name.endswith(frmt) == 1)):
                     files.append(entry.path)
                     name.append(entry.name)
-    
-#print(len(files))
+                    print(entry.name)
+
+
+
 for i in range(len(files)):
             infile = open(files[i], 'r')
             outfile = 'new_' + os.path.splitext(name[i])[0] + '.png'
             line = infile.readline()
             data = np.loadtxt(infile)
-            shift = data[:,0]
-            signal = data[:,1]
-            
+            if frmt == '.dat':
+                shift = data[:, 0]
+                signal = data[:, 2]
+            elif frmt == '.txt':
+                shift = data[:, 0]
+                signal = data[:, 1]
             spec.append(Data(shift,signal,outfile,name[i],os.path.splitext(name[i])[0]))
-            
-            
             rama.append(fittingV_ver2.Multi_fit(spec[i].shift,spec[i].signal,spec[i].name,n,width,fit_type))
 
             
-            
-if num == -1:
-    sped = str(3600)
-    print(sped)
-    for i in range(len(spec)):
-        v = sped in spec[i].name
-        #d = 'ruby_after' in spec[i].name
-        if v == True:
-            m.append(i)
-elif num == -11:
-    sped = str(500)
-    print(sped)
-    for i in range(len(spec)):
-        v = sped in spec[i].name
-        #d = 'ruby_after' in spec[i].name
-        if v == True:
-            m.append(i)
-    
-else:
-    print('\nInput keyword of spectra:\n')
-    sped = str(input())
-    for i in range(len(spec)):
-        v = sped in spec[i].name
-        #d = 'ruby_after' in spec[i].name
-        if v == True:
-            m.append(i)
-        
 
 
+print('\nInput keyword of spectra ie: (Ruby_after) or (3600cm):\n')
+sped = str(input())
+for i in range(len(spec)):
+    v = sped in spec[i].name
+    if v == True:
+        m.append(i)
 
+'''
+Change the sped == 'x' to where x is your own keyword for your file ie its central measurement wavenumber
+'''
 
-#plot.Plotter.draw_multi(rama,m)
-#for i in range(len(m)):
-
-
-#if sped == 'ruby_after':
-#    save_path = (os.getcwd() + '\\ruby_after\\' + sped + '_output data1.txt')
-#    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-#    output = open(sped + '_output data.txt','w')
-#    output.write('name\t shift\n')
-#    area,ruby = fittingV.Voigtfit.fitV(spec,n,m,rama,sped)
-#    for i in range(len(ruby)):
-#        output.write(ruby[i])
-if sped == '3600':
-    save_path = (str(os.getcwd()) + '\\3600cm\\' + sped + '_output data.txt')
+if fit_t == 2:
+    print('here_2')
+    save_path = (str(os.getcwd()) + '\\double_peak_fit\\' + sped + '_output data.txt')
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    output = open(save_path,'w')
-    output.write('name\t peak_1\t peak_2\t area_1\t area_2\t area_total\t ratio\t fwhm_1\t fwhm_2\n')
+    output = open(save_path, 'w')
+    output.write('name\t peak_1\t peak_2\t area_1\t area_2\t area_total\t ratio\tfwhm_1\t fwhm_2\n')
+
     for i in range(len(m)):
-        if n == 2:
-            compound_x,compound_y,peak1_x,peak1_fit,peak2_x,peak2_fit,total_area,area_1,area_2,ratio = fittingV_ver2.Multi_fit.peaks_2(rama[m[i]])
-        elif n == 3:
-            x,y = fittingV_ver2.Multi_fit.peaks_3(rama[m[i]])
-#    plt.plot(rama[m[0]].x,rama[m[0]].y)
-#    plt.show()
-#    plt.plot(compound_x,compound_y)
-#    plt.show()
-#    plt.plot(peak2_x,peak2_fit)
-#elif sped == '3300':
-#    print('input number of peaks to fit: \n')
-#    n = int(input())    
-#    save_path = (str(os.getcwd()) + '\\3300cm\\' + sped + '_output data.txt')
-#    os.makedirs(os.path.dirname(save_path), exist_ok=True)
-#    output = open(save_path,'w')
-#    output.write('name\t peak_1\t peak_2\t area_1\t area_2\t area_total\t ratio\t fwhm_1\t fwhm_2\n')
-#    area,ruby = fittingV.Voigtfit.fitV(spec,n,m,rama,sped)
-#    for i in range(len(area)):
-#        output.write(area[i])
-elif sped == '500':
-#   n = 3#input('input number of peaks to fit: \n')
-    save_path = (str(os.getcwd()) + '\\500cm\\' + sped + '_output data.txt')
+        save_path2 = (str(os.getcwd()) + '\\double_peak_fit\\' + rama[m[i]].title +'.txt')
+        output2 = open(save_path2, 'w')
+        output2.write('peak1_x\tpeak2_x\tpeak1_fit\tpeak2_fit\n')
+        compound_x,compound_y,peak1_x,peak1_fit,peak2_x,peak2_fit,total_area,area_1,area_2,ratio,peak1_pos,\
+        peak2_pos,fwhm_1,fwhm_2 = fittingV_ver2.Multi_fit.peaks_2(rama[m[i]])
+        output.write(rama[m[i]].title+'\t'+str(peak1_pos)+'\t'+str(peak2_pos)+'\t'+str(area_1)+'\t'+str(area_2)+'\t'+str(total_area)
+                     +'\t'+str(ratio)+'\t' + str(fwhm_1) + '\t' + str(fwhm_2) + '\n')
+
+        output2.close()
+
+elif fit_t == 3:
+    print('here_3')
+    save_path = (str(os.getcwd()) + '\\triple_peak_fit\\' + sped + '_output data.txt')
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    output = open(save_path,'w')
-    output.write('name\t area_1\t area_2\t area_total\t ratio\n')
+    output = open(save_path, 'w')
+    output.write('name\t peak_1\t peak_2\t peak_3\t area_1\t area_2\t area_3\t fwhm_1\t fwhm_2\t fwhm_3\n')
+
     for i in range(len(m)):
-        x,y,z = fittingV_ver2.Multi_fit.peaks_3(rama[m[i]])
-#    area,ruby = fittingV.Voigtfit.fitV(spec,n,m,rama,sped)
-#    for i in range(len(area)):
-#        output.write(area[i])
+        save_path2 = (str(os.getcwd()) + '\\triple_peak_fit\\' + rama[m[i]].title +'.txt')
+        output2 = open(save_path2, 'w')
+        output2.write('peak1_x\tpeak2_x\tpeak3_x\tpeak1_fit\tpeak2_fit\tpeak3_fit\n')
+
+        out = fittingV_ver2.Multi_fit.peaks_3(rama[m[i]])
+
+        output.write(rama[m[i]].title+'\t'+str(out[3])+'\t'+str(out[4])+'\t'+str(out[5])+'\t'
+                     +str(out[0])+'\t'+str(out[1])+'\t'+str(out[2])+'\t' +str(out[10])+ '\t' +
+                     str(out[11]) + '\t' + str(out[12]) + '\n')
+
+
+        output2.write(str(out[7][0]) + '\t' + str(out[8][0]) + '\t' + str(out[9][0]) + '\t'
+                          + str(out[7][1]) + '\t' + str(out[8][1]) + '\t' + str(out[9][1]) + '\n')
+        output2.close()
+
+
+
 output.close()
-    #plt.plot(x,y)
-    #plt.close()
-#root = peak_find(rama[m[0]].x,rama[m[0]].y,n)
 
+'''
+Output data is found in a folder with the keyword name
+'''
         
         
 
--- fittingV_ver2.py
+++ fittingV_ver2.py
@@ -3,6 +3,20 @@
 Created on Thu Feb 14 23:28:51 2019
 
 @author: leeva
+
+
+ADD ERROR HANDLING
+
+WHILE 
+    TRY
+    EXCEPT
+    
+change the peak find code to a function
+
+change the fitting to a function call too...maybe
+
+
+
 """
 
 import scipy
@@ -27,6 +41,20 @@ class Multi_fit():
         self.type = fit_t
     
     def peaks_2(self):
+        
+        if self.type == 1:
+            print('l')
+            lor_fit1 = LorentzianModel(prefix = 'v1_')
+            lor_fit2 = LorentzianModel(prefix = 'v2_')
+            
+        elif self.type == 2:
+            print('v')
+            lor_fit1 = VoigtModel(prefix = 'v1_')
+            lor_fit2 = VoigtModel(prefix = 'v2_')
+        elif self.type > 3:
+            print(str(self.type) + 'is not a selectable option\n')
+            
+            return 
 
         
         widths = np.arange(1,self.width,0.01)
@@ -60,7 +88,6 @@ class Multi_fit():
         
         for i in range(len(self.x)):
             if self.x[i] > (high_peak[-1][0]-fit_range) and self.x[i] < (high_peak[-1][0]+fit_range):
-                #norm = (self.y[i] - min(self.y))/(max(self.y) - min(self.y))
                 fit_X.append(self.x[i])
                 fit_Y.append(self.y[i])
         
@@ -87,15 +114,7 @@ class Multi_fit():
         
         ####_comopund fit_####
         
-        if self.type == 1:
-            print('l')
-            lor_fit1 = LorentzianModel(prefix = 'v1_')
-            lor_fit2 = LorentzianModel(prefix = 'v2_')
-            
-        elif self.type == 2:
-            print('v')
-            lor_fit1 = VoigtModel(prefix = 'v1_')
-            lor_fit2 = VoigtModel(prefix = 'v2_')
+
         
         pars = lor_fit1.guess(fit_YY,x = fit_XX)
         
@@ -140,7 +159,7 @@ class Multi_fit():
         peak1['v1_center'].set(compound_fit.params['v1_center'].value, min=compound_fit.params['v1_center'].value-5, max=compound_fit.params['v1_center'].value+5)  
         
         peak2['v2_center'].set(compound_fit.params['v2_center'].value, min=compound_fit.params['v2_center'].value-5, max=compound_fit.params['v2_center'].value+5)
-        peak2['v2_amplitude'].set(value =compound_fit.params['v2_amplitude'].value,vary = False)# ,min=3,max=7)
+        peak2['v2_amplitude'].set(value =compound_fit.params['v2_amplitude'].value,vary = False)
         peak2['v2_sigma'].set(value = compound_fit.params['v2_sigma'].value, vary = False)
         
         peak1_fit = lor_fit1.fit(fit_Y_p1,peak1,x = fit_X_p1)
@@ -151,27 +170,54 @@ class Multi_fit():
         
         plt.plot(fit_X_p1,peak1_fit.best_fit)
         plt.plot(fit_X_p2,peak2_fit.best_fit)
-
-        plt.show()
+        plt.title(str(self.title))
+        plt.savefig(str(self.title)+'.png', transparent=True)
         plt.close()
         ratio = area_1/area_2
         
         peak1_x = fit_X_p1
         peak2_x = fit_X_p2
         
-        
-        return compound_x,compound_y,peak1_x,peak1_fit.best_fit,peak2_x,peak2_fit.best_fit,total_area,area_1,area_2,ratio
+        peak1_pos = compound_fit.params['v1_center'].value
+        peak2_pos = peak2_fit.params['v2_center'].value
+
+        fwhm_1 = peak1_fit.params['v1_fwhm'].value
+        fwhm_2 = peak2_fit.params['v2_fwhm'].value
+
+        print(len(peak2_fit.best_fit))
+        print(len(peak1_x))
+
+
+        return compound_x,compound_y,peak1_x,peak1_fit.best_fit,peak2_x,peak2_fit.best_fit,total_area,\
+               area_1,area_2,ratio,peak1_pos,peak2_pos,fwhm_1,fwhm_2
             
            
-    def peaks_3(self):  
+    def peaks_3(self): 
         
-        widths=np.arange(1,self.width, 0.01)
+        """
+        Add output functionality
+        
+        """
+        
+        if self.type == 1:
+            vigot = LorentzianModel(prefix='v1_')
+            vigot2 = LorentzianModel(prefix='v2_')
+            vigot3 = LorentzianModel(prefix='v3_')
+        elif self.type == 2:
+            vigot = VoigtModel(prefix='v1_')
+            vigot2 = VoigtModel(prefix='v2_')
+            vigot3 = VoigtModel(prefix='v3_')
+        elif self.type > 3:
+            print(str(self.type) + 'is not a selectable option\n')
+            
+            return
+        
+        widths=np.arange(1,self.width, 0.001)
         
         smooth_indexes_scipy = scipy.signal.find_peaks_cwt(self.y, widths)
         peak_indexes_xs_ys = np.asarray([list(a) for a in list(zip(self.x[smooth_indexes_scipy], self.y[smooth_indexes_scipy]))])
         high_peak = sorted(peak_indexes_xs_ys, key=lambda pair: pair[1])[-self.num_peaks:]
         print('\n' + str(high_peak[-1][0]) + '\n')
-        #print(peak_indexes_xs_ys)
         print(high_peak)
 
         fit_X = []
@@ -179,8 +225,8 @@ class Multi_fit():
         fit_XX = []
         fit_YY = []
         
-        fit_range1 = 80
-        
+        fit_range1 = 500
+
         for i in range(len(self.x)):
             if self.x[i] > high_peak[-1][0]-fit_range1 and self.x[i] < high_peak[-1][0]+fit_range1:
                 x  = (self.y[i] - min(self.y))/(max(self.y) - min(self.y))
@@ -197,29 +243,16 @@ class Multi_fit():
         print(peak_indexes_xs_ys)
         print(high_peak1)
                 
-        fit_range2 = 250
+        fit_range2 = 500
             
         for i in range(len(self.x)):
             if self.x[i] > high_peak[-1][0]-fit_range2 and self.x[i] < high_peak[-1][0]+fit_range2:
                 x  = (self.y[i] - min(self.y))/(max(self.y) - min(self.y))
-                fit_X.append(self.y)
+                fit_X.append(self.x[i])
                 fit_Y.append(x)
         
         fit_X = np.asarray(fit_X)
         fit_Y = np.asarray(fit_Y)
-        
-        plt.figure()
-        plt.plot(fit_X,fit_Y)
-        plt.show()
-
-
-
-#        smooth_indexes_scipy = scipy.signal.find_peaks_cwt(fit_Y, widths)
-#        peak_indexes_xs_ys = np.asarray([list(a) for a in list(zip(fit_X[smooth_indexes_scipy], fit_Y[smooth_indexes_scipy]))])
-#        high_peak = sorted(peak_indexes_xs_ys, key=lambda pair: pair[1])[-n:]
-#        
-#        print(high_peak)
-#        print('\n' + str(high_peak[-1][0]) + '\n')
         
         
         fit_X_p1 = []
@@ -257,57 +290,67 @@ class Multi_fit():
 
         
         
-        if self.type == 1:
-            vigot = LorentzianModel(prefix='v1_')
-            vigot2 = LorentzianModel(prefix='v2_')
-            vigot3 = LorentzianModel(prefix='v3_')
-        
+
         
         pars_1 = vigot.guess(fit_Y_p1,x = fit_X_p1)
-        
         
         pars_1['v1_center'].set(high_peak[-1][0], min=high_peak[-1][0]-10, max=high_peak[-1][0]+10)
         
         out_1 = vigot.fit(fit_Y_p1,pars_1,x = fit_X_p1)
         
         
-
-        
         pars_2 = vigot2.guess(fit_Y_p2,x = fit_X_p2)
         
         pars_2['v2_center'].set(high_peak[0][0], min=high_peak[0][0]-10, max=high_peak[0][0]+10)
-        #pars['v2_sigma'].set(value = 0.2,vary = True,expr = '')
-        
+
         
         out_2 = vigot2.fit(fit_Y_p2,pars_2,x = fit_X_p2)
         
         
-
-        
         pars_3 = vigot3.guess(fit_Y_p3,x = fit_X_p3)
         
         pars_3['v3_center'].set(high_peak1[0][0], min=high_peak1[0][0]-10, max=high_peak1[0][0]+10)
-        #pars['v2_sigma'].set(value = 0.2,vary = True,expr = '')
-        
+
         
         out_3 = vigot3.fit(fit_Y_p3,pars_3,x = fit_X_p3)
         
         fit_area1 = simps(out_1.best_fit, dx=0.5)
         fit_area2 = simps(out_2.best_fit, dx=0.5)
         fit_area3 = simps(out_3.best_fit, dx=0.5)
-        
-        #area.append(str(spec[m[j]].filename) + '\t' + str(fit_area1) + '\t' + str(fit_area2) + '\t' + str(fit_area3) + '\n')
-        
-        
-        #out = mod.fit(spec[0].signal, pars, x=spec[0].shift)
-        print(out_3.fit_report(min_correl=0.25))
+
+
         plt.close()
-        #plt.plot(fit_X,fit_Y,label = self.title  + '\n peaks = ' + str(high_peak[-1][0]) + ' , ' + str(high_peak[0][0]) )
+        plt.plot(fit_X,fit_Y,label = self.title  + '\n peaks = ' + str(high_peak[-1][0]) + ' , ' + str(high_peak[0][0]) )
         plt.plot(fit_X_p1,out_1.best_fit,label = 'peak 1 best fit')
         plt.plot(fit_X_p2,out_2.best_fit,label = 'peak 2 best fit')
         plt.plot(fit_X_p3,out_3.best_fit,label = 'peak 3 best fit')
-        plt.show()
-        return fit_area1, fit_area2, fit_area3
+        plt.savefig(str(self.title)+'.png', transparent=True)
+
+        peak_1_fit = out_1.params['v1_center'].value
+        peak_2_fit = out_2.params['v2_center'].value
+        peak_3_fit = out_3.params['v3_center'].value
+
+        compound_xy = (fit_X,fit_Y)
+        fit_xy_1 = (fit_X_p1,out_1.best_fit)
+        fit_xy_2 = (fit_X_p2,out_2.best_fit)
+        fit_xy_3 = (fit_X_p3,out_3.best_fit)
+        fwhm_1 = out_1.params['v1_fwhm'].value
+        fwhm_2 = out_2.params['v2_fwhm'].value
+        fwhm_3 = out_3.params['v3_fwhm'].value
+
+
+        send_var = (fit_area1, fit_area2, fit_area3 , peak_1_fit, peak_2_fit, peak_3_fit,
+                         compound_xy, fit_xy_1, fit_xy_2, fit_xy_3,fwhm_1,fwhm_2,fwhm_3)
+
+        return send_var
+    
+    def peaks_1(self):
+        
+        return None
+    
+    def waterfall(self):
+        
+        return None
         
      
         
